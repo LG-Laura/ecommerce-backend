6c34048d6b95207d155f3fae08dffa7ecdc3e4c1
Laura Lopez <lalopezgar@gmail.com>
2024-11-27 15:28:22 -0300

    correcciones necesarias para migrar a postgres  para entorno de producción

--- config/db.js
+++ config/db.js
@@ -5,16 +5,16 @@ require('dotenv').config();
 // Crea instancia de Sequelize con los datos de conexión
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST, 
-    dialect: 'mysql',          
+    dialect: 'postgres',          
 });
 
 // Conexión a la base de datos
 const connectDB = async () => {
     try {
         await sequelize.authenticate();
-        console.log('Conexión a MySQL exitosa');
+        console.log('Conexión a PostgreSQL exitosa');
     } catch (error) {
-        console.error('Error al conectar a MySQL:', error);
+        console.error('Error al conectar a PostgreSQL:', error);
         process.exit(1);
     }
 };
